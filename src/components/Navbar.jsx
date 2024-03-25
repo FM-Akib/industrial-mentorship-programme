6e44eb4bf964f5f7154ec522ae6022e9d7ef27f0
FM-Akib <muntasirakib08@gmail.com>
2024-03-25 13:48:08 +0600

    nav bar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,46 @@
 
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import '../App.css';
+import { useState } from 'react';
+
 const Navbar = () => {
+const [open,setOpen]= useState(false);
 
-    // document.getElementById("hamburger").onclick =;
-    const handleHamburger = () => {
-        // const toggleMenu=()=> {
-            const navToggle = document.getElementsByClassName("toggle");
-            for (let i = 0; i < navToggle.length; i++) {
-              navToggle.item(i).classList.toggle("hidden");
-            }
-        //   }
-    }
-    return (
-        <nav className="flex flex-wrap items-center justify-between p-3 bg-teal-200">
-    <img src="https://tailwindflex.com/public/favicon.ico" className="h-10 w-10" alt="ACME" width="120" />
-    <div className="flex md:hidden">
-        <button id="hamburger" onClick={handleHamburger}>
-          <img className="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png" width="40" height="40" />
-          <img className="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png" width="40" height="40" />
-        </button>
+    
+return (
+    <nav className='bg-slate-50 shadow-md w-full fixed top-0 left-0'>
+       <div className="md:flex items-center justify-between bg-slate-50 py-4 md:px-60 px-7">
+
+       <div className="font-bold text-2xl cursor-pointer flex  items-center gap-1">
+            <img className=" w-24" src={logo}/>
+            {/* <span>IMP</span> */}
+        </div>
+
+
+        <div onClick={() =>setOpen(!open)} className="md:hidden absolute right-8 top-6 cursor-pointer group flex h-16 w-16  items-center justify-center rounded-3xl bg-white  hover:bg-slate-200">
+    <div className="space-y-2">
+        <span className={open?`block h-1 w-10 origin-center rounded-full bg-slate-500 transition-transform ease-in-out translate-y-1.5 rotate-45`:'block h-1 w-10 origin-center rounded-full bg-slate-500 transition-transform ease-in-out '}></span>
+        <span className={open? `block h-1 origin-center rounded-full bg-orange-500 transition-transform ease-in-out w-10 -translate-y-1.5 -rotate-45`:"block h-1 w-8 origin-center rounded-full bg-orange-500 transition-transform ease-in-out  "}></span>
     </div>
-    <div
-        className="toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 border-t-2 border-blue-900 md:border-none">
-        <a href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Home
-        </a>
-        <a href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Products
-        </a>
-        <a href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Pricing
-        </a>
-        <a href="#"
-            className="block md:inline-block text-blue-900 hover:text-blue-500 px-3 py-3 border-b-2 border-blue-900 md:border-none">Contact
-        </a>
-    </div>
-    <a href="#"
-        className="toggle hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-blue-900 hover:bg-blue-500 text-white md:rounded">Create
-        Account
+</div>
 
-    </a>
-</nav>
+        
 
+
+            <ul className={`${open? 'top-24':'top-[-490px]'} md:z-auto z-[-1] left-0 absolute md:static md:flex md:items-center pb-12 md:pb-0 bg-slate-50 pl-9 md:pl-0 transition-all duration-500 ease-in w-full md:w-auto`}>
+            <li className="md:ml-8 md:my-0 my-7 font-semibold"><Link className="text-gray-700 hover:text-yellow-500 duration-500" to="/">Home</Link></li>
+            <li className="md:ml-8 md:my-0 my-7 font-semibold"><Link className="text-gray-700 hover:text-yellow-500 duration-500" to="/">Blog</Link></li>
+            <li className="md:ml-8 md:my-0 my-7 font-semibold"><Link className="text-gray-700 hover:text-yellow-500 duration-500" to="/">Success story</Link></li>
+            <li className="md:ml-8 md:my-0 my-7 font-semibold"><Link className="text-gray-700 hover:text-yellow-500 duration-500" to="/">Registration</Link></li>
+           
+            <button type="button" className=" md:ml-8 px-8 py-3 font-semibold rounded bg-green-700 text-gray-100">Login</button>
+            </ul>
+
+
+       </div>
+        
+    </nav>
     );
 };
 
